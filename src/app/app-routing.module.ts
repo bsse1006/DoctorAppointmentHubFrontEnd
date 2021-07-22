48ddde6006dfbd299a0fff34e48a128b7885284b
bsse1006 <bsse1006@iit.du.ac.bd>
2021-07-22 08:06:35 +0600

    front end 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminAccountManagementComponent } from './admin-account-management/admin-account-management.component';
+import { AdminDoctorDashboardComponent } from './admin-doctor-dashboard/admin-doctor-dashboard.component';
+import { AdminDoctorManagementComponent } from './admin-doctor-management/admin-doctor-management.component';
+import { AdminDoctorNewComponent } from './admin-doctor-new/admin-doctor-new.component';
+import { AdminAddAdminComponent } from './admin-add-admin/admin-add-admin.component';
+import { PatientAccountManagementComponent } from './patient-account-management/patient-account-management.component';
+import { PatientAppointmentDashboardComponent } from './patient-appointment-dashboard/patient-appointment-dashboard.component';
+import { PatientDoctorAppointmentComponent } from './patient-doctor-appointment/patient-doctor-appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent, },
+  { path: 'logIn', component: LoginPageComponent, },
+  { path: 'signUp', component: SignupPageComponent, },
+  { path: 'patient/dashboard', component: PatientDashboardComponent, },
+  { path: 'admin/dashboard', component: AdminDashboardComponent, },
+  { path: 'admin/account', component: AdminAccountManagementComponent, },
+  { path: 'admin/doctors', component: AdminDoctorDashboardComponent, },
+  { path: 'admin/doctor/new', component: AdminDoctorNewComponent, },
+  { path: 'admin/doctor/:id', component: AdminDoctorManagementComponent, },
+  { path: 'admin/new', component: AdminAddAdminComponent, },
+  { path: 'patient/account', component: PatientAccountManagementComponent, },
+  { path: 'patient/appointments', component: PatientAppointmentDashboardComponent, },
+  { path: 'patient/doctor/:id', component: PatientDoctorAppointmentComponent, }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AdminNavBarComponent } from './admin-nav-bar/admin-nav-bar.component';
+import { AdminAccountManagementComponent } from './admin-account-management/admin-account-management.component';
+import { AdminDoctorDashboardComponent } from './admin-doctor-dashboard/admin-doctor-dashboard.component';
+import { AdminDoctorManagementComponent } from './admin-doctor-management/admin-doctor-management.component';
+import { AdminDoctorNewComponent } from './admin-doctor-new/admin-doctor-new.component';
+import { AdminAddAdminComponent } from './admin-add-admin/admin-add-admin.component';
+import { PatientAppointmentDashboardComponent } from './patient-appointment-dashboard/patient-appointment-dashboard.component';
+import { PatientAccountManagementComponent } from './patient-account-management/patient-account-management.component';
+import { PatientDoctorAppointmentComponent } from './patient-doctor-appointment/patient-doctor-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    AdminDashboardComponent,
+    PatientDashboardComponent,
+    NavBarComponent,
+    AdminNavBarComponent,
+    AdminAccountManagementComponent,
+    AdminDoctorDashboardComponent,
+    AdminDoctorManagementComponent,
+    AdminDoctorNewComponent,
+    AdminAddAdminComponent,
+    PatientAppointmentDashboardComponent,
+    PatientAccountManagementComponent,
+    PatientDoctorAppointmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
